8ca07dde4769887eb9f3ee6b19291ef220d34ac1
SoftBishop <reaper057@gmail.com>
2016-01-23 20:22:20 +0700

    первый

--- fixach/Program.cs
+++ fixach/Program.cs
@@ -1,17 +1,61 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace fixach
+private class Participant
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-            ///iohkigujyrtjutu
-            /// /e/rtet
-        {
-        }
+         name: string;
+         surname:string
+    }
+}
+
+public class PairParticipant
+{
+    static void Main()
+    {
+        ParParticipantFirst: Participant
+        ParticipantSecond: Participant
+    }
+}
+
+public class Competition
+{
+    static void Main()
+    {
+       name: string
+       score: int;
+    }
+}
+
+public class Judge
+{
+    static void Main()
+    {
+    Judge: Judge;
+    pair: PairParticipant;
+    Competition: Competition;
+    score: int;
+
+    }
+}
+
+public class JudgeProtocol
+{
+    static void Main()
+    {
+    Judge: Judge;
+    pair: PairParticipant;
+    Competition: Competition;
+    score: int;
+    }
+}
+public class Championship
+{
+    static void Main()
+    {
+    //pairsParticipant насчет списка хз
+    judges: Array[];
+    Competitions: Array[];
+        //JudgesProtocol too
     }
 }
